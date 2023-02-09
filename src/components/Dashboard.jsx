0d58df4d4a2087e0d830070d4965e4cc7990b7ac
tylersb <tylersb@gmail.com>
2023-02-09 13:08:30 -0800

    adds Dashboard component

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useNavigate } from 'react-router-dom'
+import { auth, db, logout } from './firebase'
+import { query, collection, getDocs, where } from 'firebase/firestore'
+import '../../styles/Dashboard.css'
+
+export default function Dashboard() {
+  const [name, setName] = useState('')
+
+  const [user, loading, error] = useAuthState(auth)
+
+  const navigate = useNavigate()
+
+  const fetchUserName = async () => {
+    try {
+      const q = query(collection(db, 'users'), where('uid', '==', user?.uid))
+      const doc = await getDocs(q)
+      const data = doc.docs[0].data()
+      setName(data.name)
+    } catch (err) {
+      console.error(err)
+      alert('An error occured while fetching user data')
+    }
+  }
+
+  useEffect(() => {
+    if (loading) return
+    if (!user) return navigate('/')
+    fetchUserName()
+  }, [user, loading])
+
+  return (
+    <div className="dashboard">
+      <div className="dashboard__container">
+        Logged in as
+        <div>{name}</div>
+        <div>{user?.email}</div>
+        <button className="dashboard__btn" onClick={logout}>
+          Logout
+        </button>
+      </div>
+    </div>
+  )
+}
