402ca5ebaa2397ce1752e994245ac754176fc177
tylersb <tylersb@gmail.com>
2023-02-09 12:58:10 -0800

    adds login form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Login from './components/Auth/Login'
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
