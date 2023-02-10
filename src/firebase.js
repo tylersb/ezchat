1159a804e2163830e853b06829a6da32b73a6432
tylersb <tylersb@gmail.com>
2023-02-09 17:13:07 -0800

    minor tweaks

--- src/firebase.js
+++ src/firebase.js
@@ -18,17 +18,16 @@ import {
   where,
   addDoc
 } from 'firebase/firestore'
-import { getDatabase, ref, set } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBxso0Zrd9u1TpRsyONP4bWMFVFzz_sq1s',
-  authDomain: 'ez-chat-b744b.firebaseapp.com',
-  databaseURL: "https://ez-chat-b744b-default-rtdb.firebaseio.com",
-  projectId: 'ez-chat-b744b',
-  storageBucket: 'ez-chat-b744b.appspot.com',
-  messagingSenderId: '101182155789',
-  appId: '1:101182155789:web:6ba2a67546e157788f7944',
-  measurementId: 'G-PFDPQ4TTZ1'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 }
 
 // Initialize Firebase
